bfe2de56484f19f9a38907a625fdf171d97d6d4f
Shengyi Wang <txyyss@gmail.com>
2018-08-29 02:22:25 +0800

    Add more examples and beautify them.
    Add M12, L_2(27),

--- CosetEnumExample.v
+++ CosetEnumExample.v
@@ -7,19 +7,25 @@ Import ListNotations.
 
 Local Open Scope positive_scope.
 
+Fixpoint flatten {A: Type} (l: list (list A)): list A :=
+  match l with
+  | nil => nil
+  | lh :: lt => lh ++ flatten lt
+  end.
+
 Section TWO_GEN_COSET_ENUM.
 
-  Inductive GenT := X | Y.
+  Inductive TwoGenT := X | Y.
 
-  Instance GenTEqDec: EqDec GenT eq.
+  Instance TwoGenTEqDec: EqDec TwoGenT eq.
   Proof.
     intros x y. unfold complement, Equivalence.equiv.
     change (x == y -> False) with (~ x == y). decide equality.
   Defined.
 
-  Instance FG_GenT: FiniteGenerators GenT.
+  Instance FG_TwoGenT: FiniteGenerators TwoGenT.
   Proof.
-    apply (Build_FiniteGenerators GenT GenTEqDec [X ; Y] 2); intros.
+    apply (Build_FiniteGenerators TwoGenT TwoGenTEqDec [X ; Y] 2); intros.
     - repeat constructor; intuition; simpl in H; intuition. discriminate.
     - destruct x; intuition.
     - simpl. reflexivity.
@@ -34,13 +40,13 @@ Section TWO_GEN_COSET_ENUM.
   Compute (map positive_to_alphabet [1; 2; 3; 4]).
 
   (* The Roration Group of a Regular Tetrahedron *)
-  Definition T_gens := [[Pe X; Pe X; Pe X]; [Pe Y; Pe Y];
-                          [Pe X; Pe Y; Pe X; Pe Y; Pe X; Pe Y]].
+  Definition T_gens := [repeat (Pe X) 3; repeat (Pe Y) 2;
+                          flatten (repeat [Pe X; Pe Y] 3)].
 
   Definition T_gens_rep :=
     Eval vm_compute in (map (map alphabet_to_positive) T_gens).
 
-  Definition T_group := standardize (compress (coset_enumration_r T_gens_rep nil 15)).
+  Definition T_group := (compress (coset_enumration_r T_gens_rep nil 16)).
 
   Lemma T_group_size: num_coset T_group == 12.
   Proof. native_compute. reflexivity. Qed.
@@ -48,13 +54,13 @@ Section TWO_GEN_COSET_ENUM.
   Compute (print_coset_table T_group).
 
   (* The Roration Group of a Regular Octahedron *)
-  Definition O_gens := [[Pe X; Pe X; Pe X; Pe X]; [Pe Y; Pe Y];
-                          [Pe X; Pe Y; Pe X; Pe Y; Pe X; Pe Y]].
+  Definition O_gens := [repeat (Pe X) 3; repeat (Pe Y) 2;
+                          flatten (repeat [Pe X; Pe Y] 4)].
 
   Definition O_gens_rep :=
     Eval vm_compute in (map (map alphabet_to_positive) O_gens).
 
-  Definition O_group := standardize (compress (coset_enumration_r O_gens_rep nil 30)).
+  Definition O_group := standardize (compress (coset_enumration_r O_gens_rep nil 35)).
 
   Compute (num_coset O_group).
 
@@ -64,8 +70,8 @@ Section TWO_GEN_COSET_ENUM.
   Compute (generator_permutations O_group).
 
   (* The Roration Group of a Regular Icosahedron *)
-  Definition I_gens := [[Pe X; Pe X; Pe X; Pe X; Pe X]; [Pe Y; Pe Y];
-                          [Pe X; Pe Y; Pe X; Pe Y; Pe X; Pe Y]].
+  Definition I_gens := [repeat (Pe X) 5; repeat (Pe Y) 2;
+                          flatten (repeat [Pe X; Pe Y] 3)].
 
   Definition I_gens_rep :=
     Eval vm_compute in (map (map alphabet_to_positive) I_gens).
@@ -80,12 +86,10 @@ Section TWO_GEN_COSET_ENUM.
   Compute (generator_permutations I_group).
 
   (* The Symmetry Group of the 24-Cell *)
-  Definition F4_gens :=
-    [[Pe X; Pe X; Pe X; Pe X; Pe X; Pe X];
-       [Pe Y; Pe Y; Pe Y; Pe Y; Pe Y; Pe Y];
-       [Pe X; Pe Y; Pe X; Pe Y; Pe X; Pe Y; Pe X; Pe Y];
-       [Pe X; Pe X; Pe X; Pe Y; Pe X; Pe X; Pe X; Pe Y];
-       [Pe X; Pe Y; Pe Y; Pe Y; Pe X; Pe Y; Pe Y; Pe Y]].
+  Definition F4_gens := [repeat (Pe X) 6; repeat (Pe Y) 6;
+                           flatten (repeat [Pe X; Pe Y] 4);
+                           flatten (repeat [Pe X; Pe X; Pe X; Pe Y] 2);
+                           flatten (repeat [Pe X; Pe Y; Pe Y; Pe Y] 2)].
 
   Definition F4_gens_rep :=
     Eval vm_compute in (map (map alphabet_to_positive) F4_gens).
@@ -97,12 +101,11 @@ Section TWO_GEN_COSET_ENUM.
   Proof. native_compute. reflexivity. Qed.
 
   (* The Symmetry Group of the 600-Cell *)
+
   Definition H4_gens :=
-    [[Pe X; Pe X; Pe X; Pe X; Pe X; Pe X; Pe X; Pe X; Pe X; Pe X];
-       [Pe Y; Pe Y; Pe Y; Pe Y; Pe Y; Pe Y];
-       [Pe X; Pe Y; Pe X; Pe Y; Pe X; Pe Y];
-       [Pe X; Pe X; Pe X; Pe X; Pe X; Pe Y; Pe X; Pe X; Pe X; Pe X; Pe X; Pe Y];
-       [Pe X; Pe Y; Pe Y; Pe Y; Pe X; Pe Y; Pe Y; Pe Y]].
+    [repeat (Pe X) 10; repeat (Pe Y) 6; flatten (repeat [Pe X; Pe Y] 3);
+       flatten (repeat (repeat (Pe X) 5 ++ [Pe Y]) 2);
+       flatten (repeat (Pe X :: repeat (Pe Y) 3) 2)].
 
   Definition H4_gens_rep :=
     Eval vm_compute in (map (map alphabet_to_positive) H4_gens).
@@ -114,11 +117,8 @@ Section TWO_GEN_COSET_ENUM.
 
   (* Mathieu group M11 *)
   Definition M11_gens :=
-    [[Pe X; Pe X]; [Pe Y; Pe Y; Pe Y; Pe Y];
-       [Pe X; Pe Y; Pe X; Pe Y; Pe X; Pe Y; Pe X; Pe Y; Pe X; Pe Y; Pe X; Pe Y; Pe X;
-          Pe Y; Pe X; Pe Y; Pe X; Pe Y; Pe X; Pe Y; Pe X; Pe Y];
-       [Pe X; Pe Y; Pe Y; Pe X; Pe Y; Pe Y; Pe X; Pe Y; Pe Y; Pe X; Pe Y; Pe Y; Pe X;
-          Pe Y; Pe Y; Pe X; Pe Y; Pe Y];
+    [repeat (Pe X) 2; repeat (Pe Y) 4; flatten (repeat [Pe X; Pe Y] 11);
+       flatten (repeat [Pe X; Pe Y; Pe Y] 6);
        [Pe X; Pe Y; Pe X; Pe Y; Pe X; Ne Y; Pe X; Pe Y; Pe X; Pe Y; Pe Y; Pe X; Ne Y;
           Pe X; Pe Y; Pe X; Ne Y; Pe X; Ne Y]].
 
@@ -132,3 +132,55 @@ Section TWO_GEN_COSET_ENUM.
   Proof. idtac "Computing M11 group...". Time native_compute. reflexivity. Qed.
 
 End TWO_GEN_COSET_ENUM.
+
+Section THREE_GEN_COSET_ENUM.
+
+  Inductive ThreeGenT := A | C | D.
+
+  Instance ThreeGenTEqDec: EqDec ThreeGenT eq.
+  Proof.
+    intros x y. unfold complement, Equivalence.equiv.
+    change (x == y -> False) with (~ x == y). decide equality.
+  Defined.
+
+  Instance FG_ThreeGenT: FiniteGenerators ThreeGenT.
+  Proof.
+    apply (Build_FiniteGenerators ThreeGenT ThreeGenTEqDec [A; C; D] 3); intros.
+    - repeat constructor; intuition; simpl in H; intuition; discriminate.
+    - destruct x; intuition.
+    - simpl. reflexivity.
+  Defined.
+
+  (* Linear Group L_2(27) *)
+  Definition L_2_27_gens :=
+    [repeat (Pe A) 3; repeat (Pe D) 13; repeat (Pe C) 2;
+       flatten (repeat [Pe D; Pe C] 2); flatten (repeat [Pe A; Pe C] 3);
+         [Ne D; Ne D; Ne D; Pe A; Pe D; Pe D; Pe D; Ne D; Ne A; Pe D; Ne A];
+         [Pe A; Ne D; Pe A; Pe D; Ne A; Ne D; Ne A; Pe D]].
+
+  Definition L_2_27_gens_rep :=
+    Eval vm_compute in (map (map alphabet_to_positive) L_2_27_gens).
+
+  (* 21743 cosets will be generated *)
+  Definition L_2_27_group := compress (coset_enumration_r L_2_27_gens_rep nil 21750).
+
+  Lemma L_2_27_group_size: num_coset L_2_27_group == 9828.
+  Proof. idtac "Compute L_2(27) group...". Time native_compute. reflexivity. Qed.
+
+  (* Mathieu Group M12 *)
+  Definition M12_gens :=
+    [repeat (Pe A) 11; repeat (Pe C) 2; repeat (Pe D) 2;
+       flatten (repeat [Pe A; Pe C] 3); flatten (repeat [Pe A; Pe D] 3);
+         flatten (repeat [Pe C; Pe D] 10);
+         [Pe A; Pe A; Pe C; Pe D; Pe C; Pe D; Pe A; Ne D; Ne C; Ne D; Ne C]].
+
+  Definition M12_gens_rep :=
+    Eval vm_compute in (map (map alphabet_to_positive) M12_gens).
+
+  (* 573191 cosets will be generated *)
+  Definition M12_group := compress (coset_enumration_r M12_gens_rep nil 573200).
+
+  Lemma M12_group_size: num_coset M12_group == 95040.
+  Proof. idtac "Computing M12 group...". Time native_compute. reflexivity. Qed.
+
+End THREE_GEN_COSET_ENUM.
