3f0c75a46e769138208b299b15569cc9b073eedf
Shengyi Wang <txyyss@gmail.com>
2022-09-08 02:33:52 -0400

    Update Category.v

--- Category/Category.v
+++ Category/Category.v
@@ -66,15 +66,6 @@ Section IDENTITY_FUNCTOR.
 
   Context `{Category C}.
 
-  (** Example 2.12.3 *)
-  Lemma arrow_eq_iff: forall {c d: C} (f g: c ~> d),
-      f = g <-> forall {X: C} (x: X ~> c), f >>> x = g >>> x.
-  Proof.
-    intros. split; intros.
-    - now rewrite H1.
-    - specialize (H1 _ cat_id). now rewrite !right_identity in H1.
-  Qed.
-
   #[global] Instance idFmap: Fmap id := fun _ _ => id.
 
   #[global] Instance idFunctor: Functor (id: C -> C) _.
@@ -623,3 +614,20 @@ Section COSLICE_CATEGORY.
   Qed.
 
 End COSLICE_CATEGORY.
+
+(** * Chapter 2.3 Generalized elements *)
+
+Section GENERALIZED_ELEMENTS.
+
+  Context `{Category C}.
+
+  (** Example 2.12.3 *)
+  Lemma arrow_eq_iff: forall {c d: C} (f g: c ~> d),
+      f = g <-> forall {X: C} (x: X ~> c), f >>> x = g >>> x.
+  Proof.
+    intros. split; intros.
+    - now rewrite H1.
+    - specialize (H1 _ cat_id). now rewrite !right_identity in H1.
+  Qed.
+
+End GENERALIZED_ELEMENTS.
